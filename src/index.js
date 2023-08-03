792b6fba8916c876cf33a773bedb6bbc1bfa0638
Rahul Kumar <rahulk28582@gmail.com>
2023-08-02 21:23:16 -0500

    fixes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';  
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -11,9 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <StateProvider initialState={initialState} reducer={reducer}>
-      <HashRouter>
         <App />
-      </HashRouter>
     </StateProvider>
   </React.StrictMode>
 );
